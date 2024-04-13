51f8f98758ccea6a2b75fcdd1d11a4a54c6241f1
Lubin Chauvreau <lubin-chauvreau@laposte.net>
2024-04-13 11:01:59 +0200

    ajout de la nouvelle feuille de script data

---
+++ script/data.js
@@ -0,0 +1,11 @@
+const restaurants =[
+  { image: "assets/images/l'abstrait.jpg"
+    nomResto:"L'abstrait"
+    description:`Restaurant bistronomique et cuisine au comptoir sont les deux concepts développés par Dorian Foulquier au 97 rue Victor-Hugo à L’Abstrait.
+    Pour vous accueillir le personnel dynamique montre un haut niveau d'hospitalité dans ce restaurant.`
+    lienWeb: "https://www.facebook.com/p/LAbstrait-100063468221757/"
+    propriete1:"Bistronomique"
+    propriete2:"Sainte foy la Grande"
+    propriete3:"Vegetarien"
+  }
+]
