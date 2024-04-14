e293f449434831a5d4f68ecb2a7b60836acd4b2b
Lubin Chauvreau <lubin-chauvreau@laposte.net>
2024-04-14 11:32:39 +0200

    version finale du système de notation spécifique à chaque article

--- script/index.js
+++ script/index.js
@@ -1,48 +1,70 @@
-const etoile0 = document.getElementById("star1")
-const etoile1= document.getElementById("star2")
-const etoile2 = document.getElementById("star3")
-const etoile3 = document.getElementById("star4")
-const etoile4 = document.getElementById("star5")
-let note = 0
-let totalNote = []
-const boutonNote = document.getElementById("note-button")
-let noteFinale = document.getElementById("note")
-let count = 0;
-const compteur = document.getElementById("compteur")
+// const etoile0 = document.querySelector(".star1")
+// const etoile1= document.querySelector(".star2")
+// const etoile2 = document.querySelector(".star3")
+// const etoile3 = document.querySelector(".star4")
+// const etoile4 = document.querySelector(".star5")
 
-const moyenne = () =>{
-  totalNote.push(note)
-}
-const nbAvis = () => {
-  count++
-  compteur.innerHTML = `${count} avis`
-}
+// etoile0.addEventListener("click", () => {
+//   note = 1
+// })
+// etoile1.addEventListener("click", () => {
+//   note = 2
+// })
+// etoile2.addEventListener("click", () => {
+//   note = 3
+// })
+// etoile3.addEventListener("click", () => {
+//   note = 4
+// })
+// etoile4.addEventListener("click", () => {
+//   note = 5
+// })
 
-  etoile0.addEventListener("click", () => {
-    note = 1
-  })
-  etoile1.addEventListener("click", () => {
-    note = 2
-  })
-  etoile2.addEventListener("click", () => {
-    note = 3
-  })
-  etoile3.addEventListener("click", () => {
-    note = 4
-  })
-  etoile4.addEventListener("click", () => {
-    note = 5
-  })
 
-// calcul de la note moyenne du restaurant
-  boutonNote.addEventListener("click", (some, notation) => {
+//Fonction notation article
+const systemeNotationArticle = (article) => {
+  // Déclaration des variables
+  const stars = Array.from(document.querySelectorAll(".star")).reverse();
+  let note = 0
+  let totalNote = []
+  const boutonNote = document.querySelector(".note-button")
+  let noteFinale = document.querySelector(".note")
+  let count = 0;
+  const compteur = document.querySelector(".compteur")
+
+  // récupération des étoiles dfans le tableau et attribution d'une note pour chacune d'entre elle
+  for (let i=0 ; i<stars.length ; i++){
+    stars[i].addEventListener("click", () => {
+      note = i + 1
+    })
+  }
+  //Fonction : compteur d'avis
+  const nbAvis = () => {
+    count++
+    compteur.innerHTML = `${count} avis`
+  }
+  //Fonction : calcul de la note moyenne du restaurant sans les décimaux (toFixed)
+  const calculNote = () => {
+    totalNote.push(note)
+    some = totalNote.reduce((acc, currentValue) => acc + currentValue)
+    notation = some / totalNote.length
+    noteFinale.innerHTML=`${notation.toFixed(0)}/5`
+    note = 0
+  }
+  // Ce qu'il se passe quand on clique sur le bouton note
+  boutonNote.addEventListener("click", () => {
     if (note !==0){
-      moyenne()
-      some = totalNote.reduce((acc, currentValue) => acc + currentValue)
-      notation = some / totalNote.length
-      console.log(notation)
-      noteFinale.innerHTML=`${notation.toFixed(1)}/5`
-      note = 0
+      calculNote()
       nbAvis() 
+    } else {
+      alert("Vous devez choisir une note de 1 a 5 étoiles")
     }
   })
+}
+// Attribution d'un système de notation spécifique à chaque article 
+const articles = document.querySelectorAll(".article");
+articles.forEach(article =>{
+  systemeNotationArticle(article)
+})
+  
+
