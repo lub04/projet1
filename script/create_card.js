393d33a00b0d51f6de28cf2e899f9961d4bca028
Lubin Chauvreau <lubin-chauvreau@laposte.net>
2024-04-18 11:24:38 +0200

    syncro code megane et lubin

---
+++ script/create_card.js
@@ -0,0 +1,82 @@
+let city = "";
+let foodtype = "";
+
+const section = document.querySelector(".articles");
+
+const createCard = (restaurant) => {
+  const article = document.createElement("article");
+  article.classList.add("article");
+
+  const cardArticle = `
+<img
+  class="img"
+  src=${restaurant.image}
+  alt=${restaurant.alt}
+/>
+<div class="description">
+  <h2 class="nom-restaurant">${restaurant.nomResto}</h2>
+  <p class="text-description">${restaurant.description}</p>
+  <p class="lien-site"
+    ><a href=${restaurant.lienWeb}>Cliquez</a> pour visiter le site !</p
+  >
+  <div class="propriete-notation">
+    <ul class="propriete">
+      <li>${restaurant.propriete1}</li>
+      <li>${restaurant.propriete2}</li>
+      <li>${restaurant.propriete3}</li>
+    </ul>
+  </div>
+  <div class="notation">
+    <button
+      class="star star5"
+      title="Donner 5 étoiles"
+      onclick="manageStars(5, ${restaurant.idResto})"
+      >☆</button
+    >
+    <button
+      class="star star4"
+      title="Donner 4 étoiles"
+      onclick="manageStars(4, ${restaurant.idResto})"
+      >☆</button
+    >
+    <button
+      class="star star3"
+      title="Donner 3 étoiles"
+      onclick="manageStars(3, ${restaurant.idResto})"
+      >☆</button
+    >
+    <button
+      class="star star2"
+      title="Donner 2 étoiles"
+      onclick="manageStars(2, ${restaurant.idResto})"
+      >☆</button
+    >
+    <button class="star star1" title="Donner 1 étoile" onclick="manageStars(1, ${restaurant.idResto})"
+      >☆</button
+    >
+  </div>
+  <div class="buton" id="compteurid_du_resto">
+    <button id="note_btn_${restaurant.idResto}" class="note-button" onclick="manageCompteur(${restaurant.idResto})">Noter !</button>
+    <p id="note_article_${restaurant.idResto}"class="note"> </p>
+    <small id="nb_avis_article_${restaurant.idResto}" class="compteur"></small>
+  </div>
+</div>`;
+
+  article.innerHTML = cardArticle;
+  return article;
+};
+
+const render = () => {
+  section.innerHTML = "";
+  for (let i = 0; i < restaurants.length; i++) {
+    if (restaurants[i].propriete2 === city || city === "") {
+      if (
+        foodtype.length === 0 ||
+        foodtype.includes(restaurants[i].propriete1)
+      ) {
+        const article = createCard(restaurants[i]);
+        section.appendChild(article);
+      }
+    }
+  }
+}
--- script/notation.js
+++ script/notation.js
@@ -1,24 +1,44 @@
-//Fonction notation article
-// const systemeNotationArticle = (article) => {
-  // Déclaration des variables
-  // const stars = Array.from(document.querySelectorAll(".star")).reverse();
-  // let note = 0
-  // let totalNote = []
-  // const boutonNote = document.querySelector(".note-button")
-  
-  // let count = 0;
-  
-  // récupération des étoiles dfans le tableau et attribution d'une note pour chacune d'entre elle
-  // for (let i=0 ; i<stars.length ; i++){
-  //   stars[i].addEventListener("click", () => {
-  //     note = i + 1
-  //   })
-  // }
-  //Fonction : compteur d'avis
- 
-  //Fonction : calcul de la note moyenne du restaurant sans les décimaux (toFixed)
+const memo = [];
 
-  // A faire au chargement de la page
+const manageStars = (idStar, idResto) => {
+  const existingResto = memo.findIndex(item => item.idResto === idResto);
+  if (existingResto !== -1) {
+    memo[existingResto].idStar = idStar;
+  } else {
+    memo.push({ idStar, idResto, compteur: 0, note: [],notation: 0 });
+  }
+}
+
+const manageCompteur = (idResto) => {
+  const existingResto = memo.findIndex(item => item.idResto === idResto);
+  if (existingResto !== -1) {
+    if (memo[existingResto].idStar !== 0) {
+      memo[existingResto].compteur += 1
+      memo[existingResto].note.push(memo[existingResto].idStar)
+      let some = memo[existingResto].note.reduce((acc, currentValue) => acc + currentValue)
+      let notation = some / memo[existingResto].note.length
+      memo[existingResto].notation = notation
+      const noteFinale = document.getElementById(`note_article_${memo[existingResto].idResto}`)
+      const compteur = document.getElementById(`nb_avis_article_${memo[existingResto].idResto}`)
+      noteFinale.innerHTML=`${notation.toFixed(1)}/5`
+      compteur.innerHTML = `${memo[existingResto].compteur} avis`
+      memo[existingResto].idStar = 0
+      
+    }else{
+      alert("Vous devez choisir une note de 1 a 5 étoiles")
+    }
+  }else{
+    alert("Vous devez choisir une note de 1 a 5 étoiles")
+  }
+  
+}
+
+  
+  
+
+  // Mise à jour dans le localstorage en stadn By
+
+    // A faire au chargement de la page
   // window.onload = () => {
   //   // récupération des notes attribuées
   //   const recupMemo = localStorage.getItem('memo_current_resto');
@@ -41,77 +61,21 @@
   //     count = compteurAvisNombre
   //   }
   // };
- 
- 
-
-
-const memo = [];
-const manageStars = (idStar, idResto) => {
-  const existingResto = memo.findIndex(item => item.idResto === idResto);
-  if (existingResto !== -1) {
-    memo[existingResto].idStar = idStar;
-  } else {
-    memo.push({ idStar, idResto, compteur: 0, note: [],notation: 0 });
-  }
-
-}
-
-
-
-const manageCompteur = (idResto) => {
-  const existingResto = memo.findIndex(item => item.idResto === idResto);
-  if (memo[existingResto].idStar !== 0) {
-    memo[existingResto].compteur += 1
-    memo[existingResto].note.push(memo[existingResto].idStar)
-    let some = memo[existingResto].note.reduce((acc, currentValue) => acc + currentValue)
-    let notation = some / memo[existingResto].note.length
-    memo[existingResto].notation = notation
-    const noteFinale = document.getElementById(`note_article_${memo[existingResto].idResto}`)
-    const compteur = document.getElementById(`nb_avis_article_${memo[existingResto].idResto}`)
-    noteFinale.innerHTML=`${notation.toFixed(1)}/5`
-    compteur.innerHTML = `${memo[existingResto].compteur} avis`
-    memo[existingResto].idStar = 0
-
-}else{
-  alert("Vous devez choisir une note de 1 a 5 étoiles")
-}
-}
-
-
-  
-  
-  const nbAvis = () => {
-    count++
-    compteur.innerHTML = `${count} avis`
-    //stockage du nombre d'avis dans le local storage
-    // 
+    // const nbAvis = () => {
+  //   count++
+  //   compteur.innerHTML = `${count} avis`
+  //   //stockage du nombre d'avis dans le local storage
+  //   // 
     
-  }
-  const calculNote = () => {
-    totalNote.push(note)
-    some = totalNote.reduce((acc, currentValue) => acc + currentValue)
-    notation = some / totalNote.length
-    noteFinale.innerHTML=`${notation.toFixed(0)}/5`
-    note = 0
-    // //stockage des notes attribuées dans le local storage
-    // localStorage.setItem('note_moyenne', notation);
-  }
-  const clickButton = (idButton) => {
+  // }
+  // const calculNote = () => {
+  //   totalNote.push(note)
+  //   some = totalNote.reduce((acc, currentValue) => acc + currentValue)
+  //   notation = some / totalNote.length
+  //   noteFinale.innerHTML=`${notation.toFixed(0)}/5`
+  //   note = 0
+  //   // //stockage des notes attribuées dans le local storage
+  //   // localStorage.setItem('note_moyenne', notation);
+  // }
 
-    if (note !==0){
-      calculNote()
-      nbAvis() 
-    } else {
-      alert("Vous devez choisir une note de 1 a 5 étoiles")
-    }
-  }
-// }
-  // mise à jour d'une variale global de type array avec les datas
-    // - vérifier si il y un objet avec l'id du resto dans le tableau (some ou find)
-  // - cas 1: jamais eu de clic pour ce resto, alros j'ajoute à mon tableau un objet .push({ restoId: id_resto, stars: numero_stars})
-  // - cas 2: il y a déjà eu un clic pour ce resto
-    // - trouver l'index de l'object findIndex() puis modifier la valeur de stars dans l'objet (memo[i].stars)
-
-
-
-  // Mise à jour dans le localstorage en stadn By
+ 
